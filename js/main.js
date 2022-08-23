24b9573049db15ede201949225cb5416818f958c
zahrneh <nehemie.zahrane@gmail.com>
2022-08-23 09:57:47 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
+ * @author  Néhémie Zahrane
  * @version 0.1
  * @since   2019-08-19
  */
@@ -8,8 +8,17 @@
 (function main() {
   'use strict'; // Demande un interprétation stricte du code
 
-  let a = 'Bonjour';
+  let TemperatureCelcius = Number( prompt('enter a température en celcius'));
+  let TemperatureFahrenheit =   TemperatureCelcius * 9 / 5 + 32;
 
-  console.log(a);
+  //alert('bonjour, ${TemperatureFahrenheit} ! ') ;
+  alert(TemperatureFahrenheit) ;
+
+
+  //[°F] = [°C] x 9 / 5 + 32
+
+
+
+
 }()); // Main IIFE
 
